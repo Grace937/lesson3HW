bf11c597dda8d934a6d28a76c9a66ee68038f600
Grace937 <87641393+Grace937@users.noreply.github.com>
2021-07-21 22:09:16 +0800

    Add files via upload

---
+++ lesson3HW_2.py
@@ -0,0 +1,26 @@
+import random
+v=random.randint(1,20)
+
+i=1
+
+g=int(input('Guess a number'))
+
+while i<5:
+    
+    if g>v:
+        print('Too large')
+        g=int(input('Try again'))
+        i=i+1
+
+    elif g<v:
+        print('Too Small')
+        g=int(input('Try again'))
+        i=i+1
+
+    elif g==v:
+        print('Correct!!!')
+        print('You guess',i,'times')
+        i=5
+        
+if i==5:
+    print('game over')
